7b2ccbacc645339194e004033fae3b2d30edd8b3
Maria Carolina <maria.b.oliveira@capgemini.com>
2023-03-27 20:46:32 -0300

    Create cypress project

--- cypress/integration/CAC.TAT spec.js
+++ cypress/integration/CAC.TAT spec.js
@@ -14,7 +14,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('#lastName').type('Oliveira')
             cy.get('#email').type('mcarolina1950@hotmail.com')
             cy.get('#open-text-area').type(longText, {delay:0})  //delay (tempo que vai demorar pra testar)
-            cy.get('button[type="submit"]').click()
+            cy.contains('button', 'Enviar').click()
 
             cy.get('.success').should('be.visible')
     })
@@ -24,7 +24,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('#lastName').type('Oliveira')
             cy.get('#email').type('mcarolina1950@hotmail,com') //email incorreto
             cy.get('#open-text-area').type('teste')
-            cy.get('button[type="submit"]').click()
+            cy.contains('button', 'Enviar').click()
 
             cy.get('.error').should('be.visible')
     })
@@ -36,13 +36,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
         cy.get('#firstName').type('Carol')
         cy.get('#lastName').type('Oliveira')
         cy.get('#email').type('mcarolina1950@hotmail.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
 
@@ -63,16 +63,120 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
-        cy.get('button[type="submit"]').click()   //clicou no botão sem preencher os campos e deu mensagem de erro
+        cy.contains('button', 'Enviar').click()   //clicou no botão sem preencher os campos e deu mensagem de erro
         cy.get('.error').should('be.visible')
     })
 
-    it.only('envia o formuário com sucesso usando um comando customizado', function(){
-        cy.fillMandatoryFieldsAndSubmit() 
+    it('envia o formuário com sucesso usando um comando customizado', function(){
+        cy.fillMandatoryFieldsAndSubmit() //executa o comando da pasta (commands.js)
 
         cy.get('.success').should('be.visible')
 
     })
+
+    it('seleciona um produto (youtube) por seu texto', function(){
+        cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
+
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function(){
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
+
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function(){
+        cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog')
+
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function(){
+        cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('have.value', 'feedback')
+
+    })
+
+    it('marca cada tipo de atendimento', function(){
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio){
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+
+        })
+
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function(){
+        cy.get('input[type="checkbox"]')
+        .check()   //check= seleciona as check boxes
+        .should('be.checked')
+        .last()    //seleciona o ultimo elemento de checkbox
+        .uncheck()  // desmarcou o que era last (telefone)
+        .should('not.be.checked')
+        
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function(){
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input){
+            //console.log($input)//
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function(){ //drag and drop: quando arrasta da pasta de docs do pc direto pro upload da pagina//
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', { action:'drag-drop'})
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+        .selectFile('@sampleFile')
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a')
+        .should('have.attr', 'target', '_blank')
+
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+        cy.get('#privacy a')
+        .invoke('removeAttr','target')
+        .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
+
+
+    })
+
+    
+
+    
+
+
+
 
 })
     
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,8 +2,7 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
         cy.get('#firstName').type('Carol')
         cy.get('#lastName').type('Oliveira')
         cy.get('#email').type('mcarolina1950@hotmail.com')
-        cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
 })
